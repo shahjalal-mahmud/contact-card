224dcf3b50527cce3f24b290aa2332299894b8d5
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2025-08-16 04:51:23 +0600

    fixed the created at time

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -197,7 +197,13 @@ export default function Profile() {
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-base-content/70" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                   </svg>
-                  {/* <span className="text-base-content/80">Joined {new Date(userProfile.createdAt?.toDate()).toLocaleDateString()}</span> */}
+                  <span className="text-base-content/80">
+                    Joined {new Date(
+                      userProfile.createdAt?.toDate
+                        ? userProfile.createdAt.toDate()
+                        : userProfile.createdAt
+                    ).toLocaleDateString()}
+                  </span>
                 </div>
                 {userProfile.education && (
                   <div className="flex items-start gap-3">
